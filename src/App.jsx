4ea37ec4f8c6bcdc61917df64b80886b7bbe741f
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-12-02 02:32:00 +0430

    set up boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
